d7aaf203424c4b995a727f8dd6c63480a06935b9
Michelle G <gerbermichelle23@gmail.com>
2019-03-20 04:33:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name){
 }
 
 function holidaysCountdown(days, holiday){
-  return `It's ${days} days until ${holiday}!`;
+  return `It\'s ${days} days until ${holiday}!`;
 }
